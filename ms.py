a15b5cf313d47c02bd4c93f7d8bbd9d9e482b311
juliankober <julian-kober@gmx.at>
2022-04-22 20:11:46 +0200

    Add winning detection

--- ms.py
+++ ms.py
@@ -10,6 +10,9 @@ import sys
 root = Tk()
 root.resizable(False, False)
 
+cheat = Toplevel(root)
+cheat.overrideredirect(True)
+cheat.geometry("1x1")
 
 mainframe = Frame(root, bg = "#d9d9d9", padx=5, pady=5)
 mainframe.grid(column=0, row=0, sticky=("N", "W", "E", "S"))
@@ -34,14 +37,16 @@ rows = 9
 cols = 9
 button_size = 20
 button_border = 3
-mines = 10
+mines = 9
 fields = []
 buttons = []
+pressed = []
 
 button_all = button_size + 2 * button_border
 
 
 for field in range(rows*cols):
+    pressed.append(False)
     if field < mines:
         fields.append(0)
     else:
@@ -52,13 +57,13 @@ random.shuffle(fields)
 for col in range(cols):
     for row in range(rows):
         if fields[col+row*rows] == None:
-            mines = 0
+            mine_count = 0
             for x in range(max(col-1, 0), min(col+1, cols - 1) + 1):
                 for y in range(max(row-1, 0), min(row+1, rows - 1) + 1):
                     if fields[x+y*rows] == 0:
-                        mines += 1
-            if mines > 0:
-                fields[col+row*rows] = mines
+                        mine_count += 1
+            if mine_count > 0:
+                fields[col+row*rows] = mine_count
 
 flag_img = Image.open("./img/Minesweeper_flag.png")
 flag_img = flag_img.resize((button_size, button_size))
@@ -66,7 +71,7 @@ mine_img = Image.open("./img/Minesweeper_mine.png")
 mine_img = mine_img.resize((button_all, button_all))
 
 empty = PhotoImage()
-pressed = PhotoImage()
+#pressed = PhotoImage()
 
 flag = ImageTk.PhotoImage(flag_img)
 mine = ImageTk.PhotoImage(mine_img)
@@ -87,39 +92,57 @@ def check_mouse_position(button):
     if mx - bx in range(button_all+5) and my - by in range(button_all+5):
         return True
 
+def update_cheat(button):
+    if fields[buttons.index(button)] == 0:
+        cheat.config(bg="#000")
+    else:
+        cheat.config(bg="#fff")
+
 def show_result(button, e = None):
     button.config(borderwidth=0,
-                  width=button_all-2,
-                  height=button_all-2,
+                  width=button_all-1,
+                  height=button_all-1,
                   activebackground="#d9d9d9")
+    button.unbind("<ButtonRelease-1")
     button.bind("<Button-1>", lambda _: "break")
+    index = buttons.index(button)
     
-    icon = fields[buttons.index(button)]
+    icon = fields[index]
     if icon is not None:
         button.config(image=icons[icon])
 
-        if icon == 0 and e:
-            print("lost!")
-            button.config(bg="#f00", activebackground="#f00")
-            for i, field in enumerate(fields):
-                if field == 0:
-                    show_result(buttons[i])
-                if buttons[i]["image"] == str(flag):
-                    buttons[i].config(bg="#f00", activebackground="#f00")
+        if icon == 0:
+            if e:
+                print("lost!")
+                for i, field in enumerate(fields):
+                    if field == 0:
+                        show_result(buttons[i])
+                    if buttons[i]["image"] == str(flag):
+                        buttons[i].config(bg="#f00", activebackground="#f00")
 
-                for b in buttons:
-                    b.bind("<Button-1>", lambda _: "break")
-                    b.unbind("<ButtonRelease-1>")
-                    b.unbind("<Button-3>")
-                    
+                    for b in buttons:
+                        if b["image"] == str(empty):
+                            b.config(activebackground="#d9d9d9")
+                        b.bind("<Button-1>", lambda _: "break")
+                        b.unbind("<ButtonRelease-1>")
+                        b.unbind("<Button-3>")
+
+                button.config(bg="#f00", activebackground="#f00")
+        else:
+            pressed[index] = True
+        
     else:
-        button.config(image=pressed)
-        col = buttons.index(button) % cols
-        row = buttons.index(button) // cols
+        pressed[index] = True
+        col = index % cols
+        row = index // cols
         for x in range(max(col-1, 0), min(col+1, cols - 1) + 1):
             for y in range(max(row-1, 0), min(row+1, rows - 1) + 1):
-                if buttons[x+y*rows]["image"] != str(pressed):
+                if not pressed[x+y*rows]:
                     show_result(buttons[x+y*rows])
+                    
+    if len([x for x in pressed if x]) == rows*cols - mines:
+        print("won!")
+        root.destroy()
 
 def toggle_flag(button):
     if button["image"] == str(empty):
@@ -136,9 +159,10 @@ for row in range(rows):
         button = Button(game, image=empty, padx=0, pady=0)
         buttons.append(button)
         button.config(bd=button_border, width=button_size, height=button_size, bg="#d9d9d9", activebackground="#ececec")
-        button.grid(column=col, row=row)
+        button.grid(column=col, row=row, sticky=("N", "W"))
         button.bind("<ButtonRelease-1>", lambda event, button=button: show_result(button, event) if check_mouse_position(button) else None)
         button.bind("<Button-3>", lambda event, button=button: toggle_flag(button))
+        button.bind("<Enter>", lambda event, button=button: update_cheat(button))
         game.columnconfigure(col, minsize=button_all+4)
     game.rowconfigure(row, minsize=button_all+4)
 
